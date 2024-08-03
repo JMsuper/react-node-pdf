f67ffcb240abd3e2ca1f3f4ae3cdf2d5cc04282e
JMsuper <tjsqls2067@naver.com>
2024-08-03 21:55:52 +0900

    [Fix] PDF 확장자 제한 & 파일명 파싱
    - PDF 확장자 제한을 위해, multer의 fileFilter 사용
    - 파일명에서 파일 경로에 혼동을 줄 수 있는 부분 "_" 처리

--- pdf-server/app.js
+++ pdf-server/app.js
@@ -19,24 +19,50 @@ app.use((req, res, next) => {
     next(); // 다음 미들웨어로 이동
 })
 
+// 파일 저장 설정
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
         const currentTime = Date.now();
-        const encodedFileName = Buffer.from(file.originalname, 'latin1').toString('utf8');
-        cb(null,"[" + currentTime + "]" + encodedFileName);
+        
+        console.log(file.originalname);
+
+        const encodedFileName = Buffer.from(file.originalname,'latin1').toString('utf8');
+
+        // 공백 및 경로에 혼동을 줄 수 있는 문자들을 밑줄로 대체
+        const parsedFileName = encodedFileName.replace(/[\s\/\\\?\%\*\:\|\<\>]/g, "_");
+        
+        cb(null,"[" + currentTime + "]" + parsedFileName);
     }
 })
 
-const upload = multer({storage: storage});
+// PDF 파일 필터링 설정
+const fileFilter = (req, file, cb) => {
+    // 허용할 파일 확장자
+    const fileTypes = /pdf/;
+    // 파일 확장자 검사
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    
+    if(extname){
+        return cb(null, true);
+    }else{
+        return cb(null, false);
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter
+});
 
 app.get('/', (req, res) => {
     res.send("Connection Test");
 });
 
 app.post('/pdf-file', upload.single('file'), (req, res) => {
+    console.log(req.file);
     if(!req.file){
         return res.status(400).send("파일 업로드 실패");
     }
