ccf0925ec9ac3b07f26ec135d3c53e4af8770ffc
JMsuper <tjsqls2067@naver.com>
2024-08-03 21:18:50 +0900

    [Add] 서버 파일 업로드 메서드 추가
    - REACT_APP_SERVER_URL : react에서 '.env' 의 값을 활용하려면, 'REACT_APP' 을 접두사로 지정해야함

--- pdf-viewer/src/components/UploadButton.js
+++ pdf-viewer/src/components/UploadButton.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const UploadButton = () => {
     const [selectedFile, setSelectedFile] = useState(null);
+    const apiUrl = process.env.REACT_APP_SERVER_URL;
 
     const handleFileUpload = (event) => {
         const file = event.target.files[0];
@@ -24,7 +25,25 @@ const UploadButton = () => {
         }
 
         if(Object.prototype.toString.call(file) === '[object File]'){
-            console.log('uploading file to server');
+            const formData = new FormData();
+            formData.append('file',file);
+
+            fetch(apiUrl + "/pdf-file", {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => {
+                if(response.ok){
+                    alert('[성공]파일 업로드 성공')
+                }else{
+                    alert('[실패]파일 업로드 실패')
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                alert('[에러]파일 업로드 중 에러 발생');
+            })
+
         }else{
             alert("파일이 유효하지 않습니다.");
         }
